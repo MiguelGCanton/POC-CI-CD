2e3bf91d6839abbda809640de118dcb10d8f3146
MiguelGcanton <miguelgcanton@pop-os.localdomain>
2021-04-23 14:53:57 -0500

    test added

--- client/src/test/java/com/pizzacli/client/ClientApplicationTests.java
+++ client/src/test/java/com/pizzacli/client/ClientApplicationTests.java
@@ -2,14 +2,16 @@ package com.pizzacli.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.Assert;
 @SpringBootTest
 class ClientApplicationTests {
 
-	// @Test
-	// void contextLoads() {
+	@Test
+	void contextLoads() {
+		String message = "joke test";
+		Assert.assertNotNull(message);
+	}
 
-	// }
 
 
 }
